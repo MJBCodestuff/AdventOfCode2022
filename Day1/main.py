a5d7051342c0071d17892acbbf399d9a461f31b4
MJBCodestuff <Manuel.B3@nurfuerspam.de>
2022-12-07 02:47:11 +0100

    day1 exercise1 complete

--- Day1/main.py
+++ Day1/main.py
@@ -1,5 +1,23 @@
-
-
-
 inputFile = open(r"input", "r")
-inputString = inputFile.
+inputString = inputFile.readlines()
+for x in range(len(inputString)):
+    inputString[x] = inputString[x].strip("\n")
+elfs = [[]]
+n = 0
+for e in inputString:
+    if e == "":
+        n += 1
+        elfs.append([])
+    else:
+        elfs[n].append(e)
+elfCalories = []
+for x in range(len(elfs)):
+    calories = 0
+    for cal in elfs[x]:
+        calories += int(cal)
+    elfCalories.append((x+1, calories))
+
+elfCalories.sort(key = lambda x: x[1])
+
+for e in elfCalories:
+    print(f"Elf {e[0]}: {e[1]}")
