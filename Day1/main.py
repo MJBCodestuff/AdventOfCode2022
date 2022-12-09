811ad71da33d204ba08e77b6562ff7aa920adebf
MJBCodestuff <Manuel.B3@nurfuerspam.de>
2022-12-09 12:37:56 +0100

    day1 exercise2 complete

--- Day1/main.py
+++ Day1/main.py
@@ -17,7 +17,17 @@ for x in range(len(elfs)):
         calories += int(cal)
     elfCalories.append((x+1, calories))
 
-elfCalories.sort(key = lambda x: x[1])
+elfCalories.sort(key = lambda x: -x[1])
+
+topThree = 0
+counter = 0
 
 for e in elfCalories:
+    if counter < 3:
+        topThree += e[1]
+        counter += 1
     print(f"Elf {e[0]}: {e[1]}")
+
+print(f"Top Three: {topThree}")
+
+
