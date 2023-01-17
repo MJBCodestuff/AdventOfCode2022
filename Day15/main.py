4ab2e2139d6057293c64ccd098559199971e916b
ManuelJBauer <Manuel.B3@nurfuerspam.de>
2023-01-17 21:54:29 +0100

    Day15 - p1 75%

--- Day15/main.py
+++ Day15/main.py
@@ -1,93 +1,73 @@
 import re
 import numpy as np
 
-CHECK_ROW = 2000000
-sensorlist = open("input").read().split("\n")
+CHECK_ROW = 10
+
+
+# Idea: For each y only note down the covered x.
+# Question: What about overlap?
+
+
+def get_manhattan_distance(a: tuple[int, int], b: tuple[int, int]):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+
+class sensor:
+
+    def __init__(self, pos, range):
+        self.position = pos
+        self.range = range
+
+    def checkIfInRange(self, point) -> bool:
+        return self.range >= get_manhattan_distance(self.position, point)
+
+    def __repr__(self):
+        return f"{self.position}, {self.range}"
+
+    def adjustPos(self, minx, miny):
+        self.position[0] = self.position[0] + minx
+        self.position[1] = self.position[1] + miny
+
+def is_Free(point):
+    for sensor in sensorlist:
+        if sensor.checkIfInRange(point):
+            return False
+    return True
+
+
+sensorlist = open("example").read().split("\n")
 remove_superfluous = re.compile(r"(Sensor at x=)|( y=)|( closest beacon is at x=)")
 sensorlist = list(map(lambda x: x.split(":"), list(map(lambda y: remove_superfluous.sub("", y), sensorlist))))
 sensorlist = list(map(lambda x: [x[0].split(","), x[1].split(",")], sensorlist))
-sensorlist = list(map(lambda x: [[int(x[0][0]), int(x[0][1])], x[1]], sensorlist))
+sensorlist = list(map(lambda x: [[int(x[0][0]), int(x[0][1])], [int(x[1][0]), int(x[1][1])]], sensorlist))
 # Sensorlist Structure: [[[Sensor x, Sensor y][Closest Beacon x, Closest Beacon y]]...]
 print(sensorlist)
 beaconlist = list(map(lambda x: [int(x[1][0]), int(x[1][1])], sensorlist))
 sensorlist = list(
-    map(lambda x: [x[0], (abs(int(x[0][0]) - int(x[1][0])) + abs(int(x[0][1]) - int(x[1][1])))], sensorlist))
+    map(lambda x: sensor(x[0], get_manhattan_distance(x[0], x[1])), sensorlist))
 print(sensorlist)
 
-
-def surroundingPoints(point, n):
-    x, y = point
-    if n == 0:
-        return point
-    else:
-        d = n
-        while d > 0:
-            yield ([x - d, y - (n - d)])
-            yield ([x + d, y - (n - d)])
-            yield ([x + d, y + (n - d)])
-            yield ([x - d, y + (n - d)])
-            for z in intermediatePoints([x - d, y - (n - d)], [x + d, y - (n - d)]):
-                for zz in z:
-                    yield zz
-            for z in intermediatePoints([x - d, y + (n - d)], [x + d, y + (n - d)]):
-                for zz in z:
-                    yield zz
-            d -= 1
-        return surroundingPoints(point, n-1)
-
-
-def intermediatePoints(pointA, pointB):
-    if pointA[0] == pointB[0]:
-        xCoords = [pointA[0]]
-    else:
-        xCoords = list(range(pointA[0], pointB[0]))
-    if pointA[1] == pointB[1]:
-        yCoords = [pointA[1]]
-    else:
-        yCoords = list(range(pointA[1], pointB[1]))
-    yield [[x, y] for x in xCoords for y in yCoords]
-
-
-notHere = []
-# really really inefficient, need to change this
-# for sensor in sensorlist:
-#     x = sensor[1]
-#     y = 0
-#     while x > 0:
-#         notHere.append([int(sensor[0][0]) - x, int(sensor[0][1]) - (sensor[1] - x)])
-#         notHere.append([int(sensor[0][0]) + x, int(sensor[0][1]) + (sensor[1] - x)])
-#         notHere.append([int(sensor[0][0]) - x, int(sensor[0][1]) + (sensor[1] - x)])
-#         notHere.append([int(sensor[0][0]) + x, int(sensor[0][1]) - (sensor[1] - x)])
-#         xintermediate = range(int(sensor[0][0]) - x, int(sensor[0][0]) + x + 1)
-#         yintermediate = range(int(sensor[0][1]) - (sensor[1] - x), int(sensor[0][1]) + (sensor[1] - x) + 1)
-#         value = [int(sensor[0][0]) - x, int(sensor[0][1]) - (sensor[1] - x)]
-#         while value != [int(sensor[0][0]) + x, int(sensor[0][1]) + (sensor[1] - x)]:
-#             for x in xintermediate:
-#                 for y in yintermediate:
-#                     value = [x, y]
-#             if value in notHere:
-#                 break
-#             notHere.append(value)
-#         x -= 1
-
-for sensor in sensorlist:
-    for x in surroundingPoints(sensor[0], sensor[1]):
-        notHere.append(x)
-print(notHere)
 
 smallestX = 0
 smallestY = 0
 maxNrX = 0
 maxNrY = 0
-for entry in notHere:
-    if entry[0] < smallestX:
-        smallestX = entry[0]
-    if entry[1] < smallestY:
-        smallestY = entry[1]
-    if entry[0] > maxNrX:
-        maxNrX = entry[0]
-    if entry[1] > maxNrY:
-        maxNrY = entry[1]
+for sensor in sensorlist:
+    pos = sensor.position
+    ran = sensor.range
+    minx = pos[0]-ran
+    miny = pos[1]-ran
+    maxx = pos[0]+ran
+    maxy = pos[1]+ran
+    if minx < smallestX:
+        smallestX = minx
+    if miny < smallestY:
+        smallestY = miny
+    if maxx > maxNrX:
+        maxNrX = maxx
+    if maxy > maxNrY:
+        maxNrY = maxy
+
 print(smallestX)
 print(smallestY)
 print(maxNrX)
@@ -98,22 +78,8 @@ maxNrX = maxNrX + toAddX
 maxNrY = maxNrY + toAddY
 print(maxNrX)
 print(maxNrY)
-notHere = list(map(lambda x: [x[0] + toAddX, x[1] + toAddY], notHere))
-beaconlist = list(map(lambda x: [x[0] + toAddX, x[1] + toAddY], beaconlist))
-sensorlist = list(map(lambda x: [int(x[0][0]) + toAddX, int(x[0][1]) + toAddY], sensorlist))
-print(notHere)
-field = np.full((maxNrX + 1, maxNrY + 1), ".")
-for entry in notHere:
-    field[entry[0]][entry[1]] = "#"
-for entry in beaconlist:
-    field[entry[0]][entry[1]] = "B"
-for entry in sensorlist:
-    field[entry[0]][entry[1]] = "S"
-print(field)
-print(sensorlist)
-print(field.shape)
-counter = 0
-for i in range(0, field.shape[0]):
-    if field[i][CHECK_ROW + toAddY] == "#":
-        counter += 1
-print(f"Result: The Row where y={CHECK_ROW} has {counter} positions where a beacon can not be present")
+for sensor in sensorlist:
+    sensor.adjustPos(toAddX, toAddY)
+
+
+#print(f"Result: The Row where y={CHECK_ROW} has {counter} positions where a beacon can not be present")
